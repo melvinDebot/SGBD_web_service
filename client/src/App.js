76c08e9777af8dcdd13c2041275f21a3ef73105d
melvin debot <melvindebot@MacBook-de-melvin.local>
2023-03-17 12:10:53 +0100

    :sparkles: add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./views/Home"
+import Table from "./views/Table"
 import Details from "./views/Details"
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
-        <Route path=":details" element={ <Details/> } />
+        <Route path=":database" element={ <Table/> } />
+        <Route path=":database/:table" element={ <Details/> } />
       </Routes>
     </div>
   )
